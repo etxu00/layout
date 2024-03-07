c5212d86c4d0af6d40c3c65d65131a509a5a06e8
etxu00 <etxu.00@gmail.com>
2024-03-07 15:53:06 -0600

    feat: :sparkles: Concepto de densidad de información
    Se grego los estilos y controles necesarios para visualizar la cantidad de espacio que ocupan los elementos

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 function init() {
   console.info("init()")
+  const $menu = document.querySelector('[data-layout="menu"]');
+  const $inputRadio = document.querySelectorAll('input[type="radio"]');
+  $inputRadio.forEach((input) => {
+    input.addEventListener('change', event => {
+      const value = event.target.value;
+      $menu.classList.remove('min', 'compact', 'normal', 'comfortable');
+      $menu.classList.add(value ? value : '');
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
